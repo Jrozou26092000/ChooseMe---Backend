12e6be576aed7b3bb6ece3089334fd6ee9620874
Juan Correa <jumcorrealo@unal.edu.co>
2021-04-22 18:54:29 -0500

    product to front

---
+++ Proyect/chooseme/src/main/java/com/chooseme/proyect/dto/ProductToFront.java
@@ -0,0 +1,51 @@
+package com.chooseme.proyect.dto;
+
+
+
+public class ProductToFront {
+
+	private String name;
+	private String brand;
+	private double price;
+	private double score;
+	private String description;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+	
+	public String setDescription() {
+		return description;
+	}
+	public void getDescription(String description) {
+		this.description = description;
+	}
+	
+		
+		
+}
+ 
